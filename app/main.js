1a76195cfdaa3a3ee3c2a9cae6840e649ceb2eac
Oshan Kottege <oshan.kottege@myob.com>
2018-03-21 17:50:19 +1300

    Added the main mark up to a new component.

--- app/main.js
+++ app/main.js
@@ -9,7 +9,7 @@ const submissionComponent = {
             <div class="content">
                 <p>
                     <strong>
-                        <a v-bind:href="submission.url" class="has-text-info">
+                        <a :href="submission.url" class="has-text-info">
                             {{ submission.title }}
                         </a>
                         <span class="tag is-small">#{{ submission.id }}</span>
@@ -19,20 +19,28 @@ const submissionComponent = {
                     <br>
                     <small class="is-size-7">
                         Submitted by:
-                        <img class="image is-24x24"
-                        v-bind:src="submission.avatar">
+                        <img class="image is-24x24" :src="submission.avatar">
                     </small>
                 </p>
             </div>
         </div>
         <div class="media-right">
-            <span class="icon is-small" v-on:click="upvote(submission.id)">
+            <span class="icon is-small" @click="upvote(submission.id)">
             <i class="fa fa-chevron-up"></i>
             <strong class="has-text-info">{{ submission.votes }}</strong>
             </span>
         </div>
     </div>
-    `
+    `,
+    props: ['submission', 'submissions'],
+    methods: {
+        upvote(submissionId) {
+            const submission = this.submissions.find(s => s.id == submissionId);
+            if(submission) {
+                submission.votes++;
+            }
+        }
+    }
 };
 
 new Vue({
@@ -44,14 +52,9 @@ new Vue({
         sortedSubmissions() {
             return this.submissions.sort((a, b) => b.votes - a.votes);
         }
-    },
-    methods: {
-        upvote(submissionId) {
-            const submission = this.submissions.find(s => s.id == submissionId);
-            if(submission) {
-                submission.votes++;
-            }
-        }
+    },    
+    components: {
+        'submission-component': submissionComponent
     }
 });
 
