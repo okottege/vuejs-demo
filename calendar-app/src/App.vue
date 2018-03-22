f0014b50e6adf96435c3d7532b5c911324c94db4
Oshan Kottege <oshan.kottege@gmail.com>
2018-03-22 22:43:25 +1300

    Started working on calendar app.

---
+++ calendar-app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app">
+        <div id="calendar-week" class="container">
+        <!-- Markup for calendar week -->
+        </div>
+        <div id="calendar-entry">
+        <!-- Markup for calendar entry -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+html, body {
+    height: 100%;
+}
+
+#app {
+    
+}
+#calendar-week {
+
+}
+#calendar-entry {
+
+}  
+</style>
+
